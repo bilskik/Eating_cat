1b341cf443a738a90e53fd2ac9155f884a0c3470
Kamil <kbilski77@gmail.com>
2022-03-04 02:09:15 +0100

    adding buttons

--- src/main.py
+++ src/main.py
@@ -17,22 +17,32 @@ class main_page_GUI():
         photo_label.place(x =350 , y = 100)
         self.buttons(window)
         window.mainloop()
-        
+   
     def buttons(self,window):
-        game_but = Button(window, text = "Nowa gra", font=('Arial',40) , bg = "black" ,fg = 'white', activebackground= 'black', activeforeground='white')
+        game_but = Button(window, text = "Nowa gra", font=('Arial',40) , bg = "black" ,fg = 'white', activebackground= 'black', activeforeground='white' , command = lambda: snake_gui(window).setup())
         game_but.place(x= 290 , y = 200)
 
-        options_but = Button(window, text = "Opcje", font=('Arial',40) , bg = "black" ,fg = 'white', activebackground= 'black', activeforeground='white')
+        options_but = Button(window, text = "Opcje", font=('Arial',40) , bg = "black" ,fg = 'white', activebackground= 'black', activeforeground='white', command = lambda: self.create_options)
         options_but.place(x= 340 , y = 300)
 
-        quit_but = Button(window, text = "Wyjście", font=('Arial',40) , bg = "black" ,fg = 'white', activebackground= 'black', activeforeground='white')
+        quit_but = Button(window, text = "Wyjście", font=('Arial',40) , bg = "black" ,fg = 'white', activebackground= 'black', activeforeground='white' , command = lambda: window.destroy())
         quit_but.place(x= 315 , y = 400)
-       
+
+    def create_options(self):
+        new_window = Toplevel()
+
+class snake_gui():
+    def __init__(self, window):
+        self.window = window
+    def setup(self):
+        new_window = Toplevel()
+
 
 
 def main():
-    main_page = main_page_GUI()
-    main_page.set_up()
+    main_win = main_page_GUI()
+    main_win.set_up()
+    
 
 
 if __name__ == "__main__":
